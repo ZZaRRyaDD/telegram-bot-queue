cd57e78519db57273d9a1b49d9e980e74b62a9a2
Nazar <odddellarobea@gmail.com>
2022-07-22 17:22:53 +0700

    delete webhooks

--- src/actions.py
+++ src/actions.py
@@ -1,33 +1,21 @@
 import logging
-import os
 
 from aiogram.utils import executor
 
 from database import init_db
 from handlers import (register_handlers_admin, register_handlers_cancel_action,
                       register_handlers_client, set_commands_client)
-from main import bot, dispatcher
-
-# webhook settings
-WEBHOOK_HOST = f'{os.getenv("WEBHOOK_HOST")}'
-WEBHOOK_PATH = ''
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = os.getenv('WEBAPP_HOST')
-WEBAPP_PORT = int(os.getenv('WEBAPP_PORT'))
+from main import dispatcher
 
 
 async def on_startup(dispatcher) -> None:
     """Action on startup app."""
     init_db()
-    await bot.set_webhook(WEBHOOK_URL)
     await set_commands_client(dispatcher)
 
 
 async def on_shutdown(dispatcher) -> None:
     """Action on shutdown app."""
-    await bot.delete_webhook()
     await dispatcher.storage.close()
     await dispatcher.storage.wait_closed()
 
@@ -37,12 +25,9 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 register_handlers_cancel_action(dispatcher)
 register_handlers_client(dispatcher)
 register_handlers_admin(dispatcher)
-executor.start_webhook(
+executor.start_polling(
     dispatcher=dispatcher,
-    webhook_path=WEBHOOK_PATH,
     skip_updates=True,
     on_startup=on_startup,
     on_shutdown=on_shutdown,
-    host=WEBAPP_HOST,
-    port=WEBAPP_PORT,
 )
