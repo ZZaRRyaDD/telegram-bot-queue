8e0294524fcb8e35f24d7a84d2e95bae7ffc7c87
Nazar <odddellarobea@gmail.com>
2022-08-05 16:37:55 +0700

    Add debug mode

--- provision/docker.py
+++ provision/docker.py
@@ -1,36 +1,43 @@
 from invoke import task
 
-START_COMMAND = "docker-compose -f bot.yml"
+START_COMMAND_DEV = "docker-compose"
+START_COMMAND_PROD = "docker-compose -f docker-compose.prod.yml"
 
 
 @task
-def build(context):
+def build(context, dev=True):
     """Build project."""
-    return context.run(f"{START_COMMAND} build")
+    return context.run(
+        f"{START_COMMAND_DEV if dev else START_COMMAND_PROD} build",
+    )
 
 
 @task
-def run(context):
+def run(context, dev=True):
     """Run postgres, redis, telegram app."""
-    return context.run(f"{START_COMMAND} up")
+    return context.run(
+        f"{START_COMMAND_DEV if dev else START_COMMAND_PROD} up",
+    )
 
 
 @task
-def run_build(context):
+def run_build(context, dev=True):
     """Run and build app."""
-    return context.run(f"{START_COMMAND} up --build")
+    return context.run(
+        f"{START_COMMAND_DEV if dev else START_COMMAND_PROD} up --build",
+    )
 
 
 @task
 def clean_volumes(context):
     """Clean volumes."""
-    return context.run(f"{START_COMMAND} down -v")
+    return context.run(f"{START_COMMAND_DEV} down -v")
 
 
 @task
 def run_container(context, command=""):
     """Base template for commands with django container."""
-    return context.run(f"{START_COMMAND} run --rm bot {command}")
+    return context.run(f"{START_COMMAND_DEV} run --rm bot {command}")
 
 
 @task
--- src/actions.py
+++ src/actions.py
@@ -1,21 +1,28 @@
 import logging
+import os
 
 from aiogram.utils import executor
 
 from database import init_db
 from handlers import (register_handlers_admin, register_handlers_cancel_action,
                       register_handlers_client, set_commands_client)
-from main import dispatcher
+from main import bot, dispatcher
+
+DEBUG = os.getenv("DEBUG", True)
 
 
 async def on_startup(dispatcher) -> None:
     """Action on startup app."""
     init_db()
     await set_commands_client(dispatcher)
+    if not DEBUG:
+        await bot.set_webhook(os.getenv("WEBHOOK_URL"))
 
 
 async def on_shutdown(dispatcher) -> None:
     """Action on shutdown app."""
+    if not DEBUG:
+        await bot.delete_webhook()
     await dispatcher.storage.close()
     await dispatcher.storage.wait_closed()
 
@@ -25,9 +32,27 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 register_handlers_cancel_action(dispatcher)
 register_handlers_client(dispatcher)
 register_handlers_admin(dispatcher)
-executor.start_polling(
-    dispatcher=dispatcher,
-    skip_updates=True,
-    on_startup=on_startup,
-    on_shutdown=on_shutdown,
-)
+
+
+def main() -> None:
+    if DEBUG:
+        executor.start_polling(
+            dispatcher=dispatcher,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+        )
+    else:
+        executor.start_webhook(
+            dispatcher=dispatcher,
+            webhook_path=os.getenv("WEBHOOK_PATH"),
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=os.getenv("WEBAPP_HOST"),
+            port=int(os.getenv("WEBAPP_PORT")),
+        )
+
+
+if __name__ == "__main__":
+    main()
