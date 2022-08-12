36e4c2a026b241a1b94641e05b5924d43ebce737
Nazar <yfurnoxl@yandex.ru>
2022-08-12 05:02:30 +0000

    Remove redis storage

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
 import os
 
 from aiogram import Bot
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 
 bot: Bot = Bot(token=os.getenv("TOKEN"))
-dispatcher: Dispatcher = Dispatcher(bot, storage=RedisStorage2(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
-    db=5,
-    pool_size=10,
-))
+dispatcher: Dispatcher = Dispatcher(bot, storage=MemoryStorage())
