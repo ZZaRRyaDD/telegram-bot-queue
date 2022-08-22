e78cd95670d994f97af89404dfaeabdebfffef5f
Nazar <yfurnoxl@yandex.ru>
2022-08-22 04:34:14 +0000

    Try add webhooks

--- src/actions.py
+++ src/actions.py
@@ -1,15 +1,24 @@
 import logging
 import os
+import asyncio
 
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram import Dispatcher, types
 
 from database import init_db
 from handlers import (register_handlers_admin, register_handlers_cancel_action,
                       register_handlers_client, set_commands_client)
 from main import bot, dispatcher
 
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG") != "False"
+# DEBUG = True
+
+
+@dispatcher.message_handler()
+async def echo(message: types.Message):
+    return SendMessage(message.from_user.id, message.text)
 
 
 async def on_startup(dispatcher) -> None:
@@ -17,7 +26,7 @@ async def on_startup(dispatcher) -> None:
     init_db()
     await set_commands_client(dispatcher)
     if not DEBUG:
-        await bot.set_webhook(os.getenv("WEBHOOK_URL"))
+        await bot.set_webhook(url=os.getenv("WEBHOOK_URL"))
     await bot.send_message(int(os.getenv("ADMIN_ID")), "Я запущен")
 
 
@@ -40,20 +49,26 @@ register_handlers_cancel_action(dispatcher)
 register_handlers_client(dispatcher)
 register_handlers_admin(dispatcher)
 
-if DEBUG:
-    executor.start_polling(
-        dispatcher=dispatcher,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-    )
-else:
-    executor.start_webhook(
-        dispatcher=dispatcher,
-        webhook_path=os.getenv("WEBHOOK_PATH"),
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=os.getenv("WEBAPP_HOST"),
-        port=int(os.getenv("WEBAPP_PORT")),
-    )
+
+def start():
+    if DEBUG:
+        executor.start_polling(
+            dispatcher=dispatcher,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+        )
+    else:
+        executor.start_webhook(
+            dispatcher=dispatcher,
+            webhook_path=os.getenv("WEBHOOK_PATH"),
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=os.getenv("WEBAPP_HOST"),
+            port=int(os.getenv("WEBAPP_PORT")),
+        )
+
+
+if __name__ == "__main__":
+    start()
