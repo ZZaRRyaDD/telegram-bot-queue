fa37cddffe021e64bc56f02438cb71589be840c3
Nazar <odddellarobea@gmail.com>
2022-08-27 23:35:24 +0700

    Remove not worked webhooks

--- src/actions.py
+++ src/actions.py
@@ -1,37 +1,24 @@
 import logging
 import os
-import asyncio
-import ssl
-from aiohttp import web
+
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
-from aiogram.dispatcher.webhook import get_new_configured_app
 
 from database import init_db
 from handlers import (register_handlers_admin, register_handlers_cancel_action,
                       register_handlers_client, set_commands_client)
 from main import bot, dispatcher
 
-DEBUG = os.getenv("DEBUG") != "False"
-
 
 async def on_startup(_) -> None:
     """Action on startup app."""
     init_db()
     await set_commands_client(dispatcher)
-    if not DEBUG:
-        await bot.set_webhook(
-            url=f"{os.getenv('IP')}{os.getenv('WEBHOOK_PATH')}",
-            certificate=open(WEBHOOK_SSL_CERT, 'rb'),
-        )
-        await bot.send_message(int(os.getenv("ADMIN_ID")), await bot.get_webhook_info())
     await bot.send_message(int(os.getenv("ADMIN_ID")), "Я запущен")
 
 
 async def on_shutdown(_) -> None:
     """Action on shutdown app."""
-    if not DEBUG:
-        await bot.delete_webhook()
     await dispatcher.storage.close()
     await dispatcher.storage.wait_closed()
     await bot.send_message(int(os.getenv("ADMIN_ID")), "Я отрубаюсь")
@@ -48,20 +35,9 @@ register_handlers_client(dispatcher)
 register_handlers_admin(dispatcher)
 
 
-if DEBUG:
-    executor.start_polling(
-        dispatcher=dispatcher,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-    )
-else:
-    executor.start_webhook(
-        dispatcher=dispatcher,
-        webhook_path=os.getenv("WEBHOOK_PATH"),
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=os.getenv("WEBAPP_HOST"),
-        port=int(os.getenv("WEBAPP_PORT")),
-    )
+executor.start_polling(
+    dispatcher=dispatcher,
+    skip_updates=True,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+)
